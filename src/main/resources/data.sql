f0cbbcbc9fccbaf191be1e842cc6130e670032a1
Mahtab Alam <Mahtab.Alam@ge.com>
2022-06-01 13:04:55 +0530

    added routes table and added entries

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,27 @@
 INSERT INTO STOPS (id, stop) VALUES
   (1, 'Majestic'),
-  (2, 'Silk Board'),
+  (2, 'Central Silk Board'),
   (3, 'Shivaji Nagar'),
   (4, 'BEML Layout'),
-  (5, 'Marathahalli Bridge');
+  (5, 'Kundanahalli Gate'),
+  (6, 'Spice Garden'),
+  (7, 'Marathahalli Bridge'),
+  (8, 'Innovative Multiplex'),
+  (9, 'Kadubeesanahalli'),
+  (10, 'New Horizon College'),
+  (11, 'Devarabisanahalli '),
+  (12, 'Eco Space'),
+  (13, 'Bellandur Central Mall'),
+  (14, 'Bellandur Petrol Bunk'),
+  (15, 'Sarjapura Junction'),
+  (16, 'Ibbaluru'),
+  (17, 'Agara'),
+  (18, 'Depot 25'),
+  (19, 'HSR Layout'),
+  (20, 'HSR Layout S.I. Apartment');
+
+
+INSERT INTO ROUTES (id, name, stops) VALUES
+  (1, '500CK', (1,2));
+
+
